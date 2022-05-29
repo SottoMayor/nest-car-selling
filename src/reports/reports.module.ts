059225962853ec53f4a289838842f55214370a79
David Sotto Mayor <ddsmmf@gmail.com>
2022-05-29 18:27:56 -0400

    Setting up and Entity and Repository(Reports)

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Report } from './reports.entity';
 
 @Module({
   controllers: [ReportsController],
   providers: [ReportsService],
   exports: [ReportsService],
+  imports: [TypeOrmModule.forFeature([Report])]
 })
 export class ReportsModule {}
