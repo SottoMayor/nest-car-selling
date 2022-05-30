433244f03fcbadcfe4efeb7b9bfa2b813a95f676
David Sotto Mayor <ddsmmf@gmail.com>
2022-05-30 16:16:47 -0400

    Creating and saving users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from './dtos/CreateUser.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService){}
+
   @Post('/signup')
   async createUser(@Body() body: CreateUserDto) {
-    const { email, password } = body;
-    console.log(body);
-    
+    return this.usersService.create(body);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
+
+type CreateUserType = {
+    email: string
+    password: string
+}
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(User) private userRepository: Repository<User>){}
+
+    create(body: CreateUserType){
+        const { email, password } = body;
+
+        const user = this.userRepository.create({ email, password });
+
+        return this.userRepository.save(user);
+    }
+
+}
