8f88a6be8ccd94dce10bec9e349fd31c75840e16
David Sotto Mayor <ddsmmf@gmail.com>
2022-05-31 10:34:37 -0400

    Building services to query users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './users.entity';
@@ -20,4 +20,23 @@ export class UsersService {
         return this.userRepository.save(user);
     }
 
+    findById(id: number) {
+        const user = this.userRepository.findOneBy({ id });
+        if(!user){
+            throw new NotFoundException('This user does not exist.');
+        }
+
+        return user;
+    }
+
+    find(email: string) {
+        // If not exists, this will return a empty array...
+        const users = this.userRepository.find({ where: { email } });
+
+        return users;
+    }
+
+    updated() {}
+
+    remove() {}
 }
