f06c002fd3f2794952d2d6306a1b661b7f6faafd
David Sotto Mayor <ddsmmf@gmail.com>
2022-06-14 17:41:03 -0400

    Setting env var to Cookie-Session hash functionality

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 const cookieSession = require('cookie-session');
+import { ConfigModule } from '@nestjs/config';
+
+// This way we can set env vars with out call the ConfigService;
+ConfigModule.forRoot();
+//console.log(process.env.COOKIE_KEY)
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
   app.use(cookieSession({
-    keys: ['ThisWillBeUsedToEncryptOurCookies']
+    keys: [process.env.COOKIE_KEY]
   }))
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
