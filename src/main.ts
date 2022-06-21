fb861af39b2e0ec7c338841c462ff463dc483130
David Sotto Mayor <ddsmmf@gmail.com>
2022-06-21 09:41:06 -0400

    Working with swagger custom options

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 const cookieSession = require('cookie-session');
 import { ConfigModule } from '@nestjs/config';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder, SwaggerCustomOptions } from '@nestjs/swagger';
+import { join } from 'path';
 
 // This way we can set env vars with out call the ConfigService;
 ConfigModule.forRoot();
@@ -11,7 +13,10 @@ ConfigModule.forRoot();
 
 async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule);
+  const app: NestExpressApplication = await NestFactory.create(AppModule);
+
+  // Serving files in a static folder
+  app.useStaticAssets(join(__dirname, '..', 'assets'))
 
   app.use(cookieSession({
     keys: [process.env.COOKIE_KEY]
@@ -19,6 +24,7 @@ async function bootstrap() {
   
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
+  // Swagger configs...
   const config = new DocumentBuilder()
     .setTitle('Car Selling')
     .setDescription('Test API built with NestJs. The general idea is publish car ads.')
@@ -26,7 +32,14 @@ async function bootstrap() {
     .addTag('Cars')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+    customSiteTitle: 'Car Selling Docs',
+    customfavIcon: '../favicon.ico'
+  };
+  SwaggerModule.setup('api', app, document, customOptions);
 
   await app.listen(3000);
 }
