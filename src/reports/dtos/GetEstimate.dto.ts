01a84c8d3a989f9574cf4183b7b9574135c17100
David Sotto Mayor <ddsmmf@gmail.com>
2022-06-26 22:14:55 -0400

    Documenting all the reports module

--- src/reports/dtos/GetEstimate.dto.ts
+++ src/reports/dtos/GetEstimate.dto.ts
@@ -2,16 +2,28 @@ import { Transform } from "class-transformer";
 import { IsString, IsNumber, IsNotEmpty, IsInt, IsPositive, IsAlpha, IsLatitude, IsLongitude, Min, Max } from "class-validator";
 
 export class GetEstimateDto {
+    /**
+     * The manufacturer which built the car
+     * @example honda
+    */
     @IsNotEmpty()
     @IsString()
     @IsAlpha()
     make: string;
 
+    /**
+     * The model car
+     * @example civic
+    */
     @IsNotEmpty()
     @IsString()
     @IsAlpha()
     model: string;
 
+     /**
+     * The year which the car was built
+     * @example 2020
+    */
     @Transform(({ value }) => parseInt(value))
     @IsNotEmpty()
     @IsNumber()
@@ -20,6 +32,10 @@ export class GetEstimateDto {
     @Min(2000)
     year: number;
     
+    /**
+     * The total amount of mileage that the car toured
+     * @example 10000
+    */
     @Transform(({ value }) => parseInt(value))
     @IsNotEmpty()
     @IsNumber()
@@ -27,11 +43,19 @@ export class GetEstimateDto {
     @Max(1000000)
     mileage: number;
     
+    /**
+     * The total amount of longitute distance available to search cars.
+     * @example 100
+    */
     @Transform(({ value }) => parseFloat(value))
     @IsNotEmpty()
     @IsLongitude()
     lng: number;
     
+     /**
+     * The total amount of latitude distance available to search cars.
+     * @example 100
+    */
     @Transform(({ value }) => parseFloat(value))
     @IsNotEmpty()
     @IsLatitude()
--- src/reports/dtos/Reports.dto.ts
+++ src/reports/dtos/Reports.dto.ts
@@ -1,30 +1,66 @@
 import { Expose, Transform } from "class-transformer";
 
 export class ReportsDto {
+    /**
+     * The manufacturer which built the car
+     * @example honda
+    */
     @Expose()
     make: string;
     
+    /**
+     * The model car
+     * @example civic
+    */
     @Expose()
     model: string;
     
+     /**
+     * The year which the car was built
+     * @example 2020
+    */
     @Expose()
     year: number;
     
+    /**
+     * The total amount of mileage that the car toured
+     * @example 10000
+    */
     @Expose()
     mileage: number;
-    
+
+    /**
+     * The total amount of longitute distance available to search cars.
+     * @example 100
+    */
     @Expose()
     lng: number;
     
+     /**
+     * The total amount of latitude distance available to search cars.
+     * @example 100
+    */
     @Expose()
     lat: number;
     
+    /**
+     * The car price.
+     * @example 109000
+    */
     @Expose()
     price: number;
 
+    /**
+     * The condition was approved.
+     * @example true
+    */
     @Expose()
     approved: boolean
     
+    /**
+     * The user id.
+     * @example 1
+    */
     @Transform(({ obj }) => obj.user.id)
     @Expose()
     userId: number;
